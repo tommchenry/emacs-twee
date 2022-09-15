12a2f8635e0dbc99804b950114220ec842628ca8
Tom McHenry <6570644+tommchenry@users.noreply.github.com>
2022-09-14 21:36:55 -0500

    Test push

--- twee.el
+++ twee.el
@@ -1,8 +1,8 @@
 ;; A major mode for Twee
 
 (setq twee-highlights
-      '(("Sin\\|Cos\\|Sum" . 'font-lock-function-name-face)
-        ("Pi\\|Infinity" . 'font-lock-constant-face)))
+      '(("StoryData\\|StoryTitle\\|Start" . 'font-lock-function-name-face)
+        ("true\\|false" . 'font-lock-constant-face)))
 
 (define-derived-mode twee-mode fundamental-mode "twee"
   "major mode for editing twee language code."
